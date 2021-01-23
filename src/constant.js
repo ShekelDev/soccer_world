b97152e61384ebcb90f43048e8a298bd409335bc
ShekelDev <77896972+ShekelDev@users.noreply.github.com>
2021-01-23 21:01:49 +0200

    Delete constant.js

--- src/constant.js
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_API_KEY = "AIzaSyBTVtcYrKvr3jmR4aP2D0Ik62ERXU_xyYU";
-export const RAPID_API_KEY = "be293ff923msha473befedef4bd6p121ee0jsn1c192ba54b5d";
-export const RAPID_API_HOST = "api-football-v1.p.rapidapi.com";
-export const URL = { leagues: "leagues", standings: "standings" };
