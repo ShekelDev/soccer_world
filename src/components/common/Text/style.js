4a01c4393c45555950f0deac574d02d4ce97b612
Omer Bresinski <omer@powerlink.co.il>
2021-01-23 20:40:06 +0200

    Ellipsis

--- src/components/common/Text/style.js
+++ src/components/common/Text/style.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
-export const BaseText = styled.span`
+export const BaseText = styled.div`
     font-size: ${({ size }) => size || "14px"};
     font-weight: ${({ isBold }) => (isBold ? 700 : 400)};
     color: ${({ color }) => color || "292929"};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 `;
